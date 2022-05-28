8704456245dffb8caf5f160e4fa8a0874db515e9
Lewe Ohlsen <lewe.ohlsen@mindpeak.ai>
2022-05-29 00:50:52 +0200

    WIP on nice transactions timeline

--- src/components/TransactionsList/index.tsx
+++ src/components/TransactionsList/index.tsx
@@ -1,15 +1,59 @@
 import { useSelector } from "react-redux";
+import { Timeline, Card } from "antd";
+import { ShoppingCartOutlined, UserAddOutlined, UsergroupAddOutlined } from "@ant-design/icons";
 import { selectTransactions } from "../../app/events.slice";
+import { Transaction, Purchase } from "../../app/types";
+import ShoppingCart from "../ShoppingCart";
 
-const TransactionsList = () => {
+const TransactionsList: React.FC = () => {
     const transactions = useSelector(selectTransactions);
 
+    const renderTransactionIcon = (transactionType: string) => {
+        switch (transactionType) {
+            case "events/checkout":
+                return <ShoppingCartOutlined />;
+            case "events/addAccount":
+                return <UserAddOutlined />;
+            case "events/addGroup":
+                return <UsergroupAddOutlined />;
+
+            default:
+                return null;
+                break;
+        }
+    };
+
+    // const renderPurchase = (purchase: Purchase) => {
+    //     return null;
+    // }
+
+    // const renderTransactionPayload = (transaction: Transaction) => {
+    //     switch (transaction.type) {
+    //         case "events/checkout":
+    //             return renderPurchase(transaction.payload)
+    //             break;
+
+    //         default:
+    //             break;
+    //     }
+    // }
+
     return (
-        <div>
-            {transactions.map((transaction) => (
-                <p>{JSON.stringify(transaction)}</p>
-            ))}
-        </div>
+        <Card>
+            <Timeline mode="left">
+                {transactions.map((transaction) => (
+                    <Timeline.Item
+                        key={transaction.timestamp}
+                        dot={<div style={{ fontSize: "2em" }}>{renderTransactionIcon(transaction.type)}</div>}
+                        label={new Date(transaction.timestamp).toISOString()}
+                        color="red"
+                    >
+                        {JSON.stringify(transaction)}
+                        {/* renderTransactionPayload(transaction) */}
+                    </Timeline.Item>
+                ))}
+            </Timeline>
+        </Card>
     );
 };
 
